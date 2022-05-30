c3f5c4f6f6d38c28444dd5c2eda369bfee82fd50
Liakat21 <jupiterali1990@gmail.com>
2022-05-30 13:46:20 +0600

    css add

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Home() {
         <meta name="description" content="Online Shop In Bangladesh" />
         <link rel="icon" href="/favicon.ico" />
 
-        <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
-        </Head>  
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigi="anonymous"/>
 
+      </Head>       
       <Featured/>
       <ProductList/>
       <Clients/>
