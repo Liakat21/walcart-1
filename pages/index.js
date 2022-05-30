1143a04327f4b98d38c4dedd587aadc17fc48b6c
Liakat21 <jupiterali1990@gmail.com>
2022-05-30 13:18:11 +0600

    add somethih

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Clients from '../component/Clients'
 import Featured from '../component/Featured'
 import ProductList from '../component/ProductList'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import styles from '../styles/Home.module.css'
@@ -16,8 +18,10 @@ export default function Home() {
         <title>Walcart</title>
         <meta name="description" content="Online Shop In Bangladesh" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-      </Head>       
+
+        <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+        </Head>  
+
       <Featured/>
       <ProductList/>
       <Clients/>
